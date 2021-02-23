40562336ca6fcb5ccb2137694ae5eb08f7bac715
TehBrian <tehbrian@outlook.com>
2021-02-23 16:57:45 -0500

    paper: Add RequestType to RawNoteRequest

--- server-paper/src/main/java/xyz/tehbrian/mcmidi/serverpaper/RawNoteRequest.java
+++ server-paper/src/main/java/xyz/tehbrian/mcmidi/serverpaper/RawNoteRequest.java
@@ -1,19 +1,22 @@
 package xyz.tehbrian.mcmidi.serverpaper;
 
 import xyz.tehbrian.mcmidi.serverapi.Note;
+import xyz.tehbrian.mcmidi.serverapi.RequestType;
 
 /**
  * Represents a request for a note to be played by some player as received
- * directly from an API request. Named "raw" because there is no definite {@link org.bukkit.entity.Player}
- * attached to this request yet.
+ * directly from a client. There is no definite {@link org.bukkit.entity.Player}
+ * attached yet.
  */
 public final class RawNoteRequest {
 
     public final String playerName;
+    public final RequestType type;
     public final Note note;
 
-    public RawNoteRequest(final String playerName, final Note note) {
+    public RawNoteRequest(final String playerName, final RequestType type, final Note note) {
         this.playerName = playerName;
+        this.type = type;
         this.note = note;
     }
 
@@ -24,6 +27,15 @@ public final class RawNoteRequest {
      */
     public String getPlayerName() {
         return this.playerName;
+    }
+
+    /**
+     * Gets the type of request.
+     *
+     * @return the request type
+     */
+    public RequestType getType() {
+        return this.type;
     }
 
     /**
