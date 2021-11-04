3a7a0d22c22138fc2b28a2380a65826c5103b320
TehBrian <tehbrian@protonmail.com>
2021-11-04 19:43:25 -0400

    revert Note and RawNoteRequest to non-record classes

--- server-api/src/main/java/xyz/tehbrian/mcmidi/serverapi/Note.java
+++ server-api/src/main/java/xyz/tehbrian/mcmidi/serverapi/Note.java
@@ -2,13 +2,38 @@ package xyz.tehbrian.mcmidi.serverapi;
 
 /**
  * Represents a note that can be played.
- *
- * @param instrument the instrument
- * @param pitch      the pitch
- * @param velocity   the velocity represented as a float from 0 to 1
  */
-public record Note(Instrument instrument,
-                   Pitch pitch,
-                   float velocity) {
+public final class Note {
+
+    private final Instrument instrument;
+    private final Pitch pitch;
+    private final float velocity;
+
+    public Note(final Instrument instrument, final Pitch pitch, final float velocity) {
+        this.instrument = instrument;
+        this.pitch = pitch;
+        this.velocity = velocity;
+    }
+
+    /**
+     * @return the instrument
+     */
+    public Instrument instrument() {
+        return this.instrument;
+    }
+
+    /**
+     * @return the pitch
+     */
+    public Pitch pitch() {
+        return this.pitch;
+    }
+
+    /**
+     * @return the velocity represented as a float from 0 to 1
+     */
+    public float velocity() {
+        return this.velocity;
+    }
 
 }
--- server-paper/src/main/java/xyz/tehbrian/mcmidi/serverpaper/RawNoteRequest.java
+++ server-paper/src/main/java/xyz/tehbrian/mcmidi/serverpaper/RawNoteRequest.java
@@ -8,13 +8,38 @@ import xyz.tehbrian.mcmidi.serverapi.RequestType;
  * a client.
  * <p>
  * There is no definite {@link org.bukkit.entity.Player} attached yet.
- *
- * @param playerName the player requesting the note
- * @param type       the request type
- * @param note       the note
  */
-public record RawNoteRequest(String playerName,
-                             RequestType type,
-                             Note note) {
+public final class RawNoteRequest {
+
+    public final String playerName;
+    public final RequestType type;
+    public final Note note;
+
+    public RawNoteRequest(final String playerName, final RequestType type, final Note note) {
+        this.playerName = playerName;
+        this.type = type;
+        this.note = note;
+    }
+
+    /**
+     * @return the player requesting the note
+     */
+    public String playerName() {
+        return this.playerName;
+    }
+
+    /**
+     * @return the request type
+     */
+    public RequestType type() {
+        return this.type;
+    }
+
+    /**
+     * @return the note
+     */
+    public Note note() {
+        return this.note;
+    }
 
 }
