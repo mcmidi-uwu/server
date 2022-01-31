783d34c96eb033db74aa40fab8e787b4bc84a61b
TehBrian <tehbrian@protonmail.com>
2022-01-31 14:30:04 -0500

    build[api]: use indra for publishing, publish to thbn

--- server-api/build.gradle.kts
+++ server-api/build.gradle.kts
@@ -1,8 +1,8 @@
 plugins {
     `java-library`
-    `maven-publish`
     id("net.kyori.indra") version "2.0.6"
     id("net.kyori.indra.checkstyle") version "2.0.6"
+    id("net.kyori.indra.publishing") version "2.0.6"
 }
 
 group = rootProject.group
@@ -18,10 +18,33 @@ dependencies {
     compileOnly("io.papermc.paper:paper-api:1.18.1-R0.1-SNAPSHOT")
 }
 
-publishing {
-    publications {
-        create<MavenPublication>("maven") {
-            from(components["java"])
+indra {
+    javaVersions {
+        target(17)
+    }
+
+    mitLicense()
+
+    publishReleasesTo("thbn", "https://repo.thbn.me/releases")
+    publishSnapshotsTo("thbn", "https://repo.thbn.me/snapshots")
+
+    configurePublications {
+        pom {
+            url.set("https://github.com/TehBrian/tehlib")
+
+            developers {
+                developer {
+                    id.set("TehBrian")
+                    url.set("https://tehbrian.xyz")
+                    email.set("tehbrian@protonmail.com")
+                }
+            }
+
+            scm {
+                connection.set("scm:git:git://https://github.com/TehBrian/tehlib.git")
+                developerConnection.set("scm:git:ssh://https://github.com/TehBrian/tehlib.git")
+                url.set("https://github.com/TehBrian/tehlib.git")
+            }
         }
     }
 }
